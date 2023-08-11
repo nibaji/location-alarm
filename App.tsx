0266d7727d9af8202bd6049ce17d8dd27adb5549
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2023-08-12 03:28:52 +0530

    use webview and google maps to get the target gps coordinates

--- App.tsx
+++ App.tsx
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Button, View, Text } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [url, setUrl] = useState<string | undefined>(
+		"https://www.google.com/maps"
+	);
+
+	const getGpsCoordinates = () => {
+		const regex = /@(-?\d+\.\d+),(-?\d+\.\d+)/;
+		const match = url?.match(regex);
+		if (match) {
+			const latitude = parseFloat(match[1]);
+			const longitude = parseFloat(match[2]);
+			console.log({ latitude, longitude });
+			return { latitude, longitude };
+		} else {
+			return null;
+		}
+	};
+
+	useEffect(() => {
+		getGpsCoordinates();
+	}, [url]);
+
+	return (
+		<View style={styles.container}>
+			<WebView
+				style={styles.webView}
+				source={{ uri: "https://www.google.com/maps" }}
+				scrollEnabled
+				geolocationEnabled
+				androidLayerType="hardware"
+				originWhitelist={["https://www.google.com/maps"]}
+				onNavigationStateChange={(e) => setUrl(e.url)}
+			/>
+			<View style={styles.buttonWrapper}>
+				<Button title="Pick the chosen location" color="red" />
+				<Text>Search a location or Pick in the map</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	webView: {
+		flex: 1,
+		width: 400,
+	},
+	buttonWrapper: {
+		flex: 0.16,
+		backgroundColor: "purple",
+		justifyContent: "center",
+		width: "100%",
+		padding: 16,
+	},
 });
